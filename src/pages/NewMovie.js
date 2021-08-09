40f58cde32e80f087c86e4efa797b8cf511183ce
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-08-09 12:36:02 -0300

    testando requisito 1

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,7 +1,41 @@
+// import React, { Component } from 'react';
+// import { Redirect } from 'react-router';
+
+// import MovieForm from '../components/MovieForm';
+// import * as movieAPI from '../services/movieAPI';
+
+// class NewMovie extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state({
+//       shouldRedirect: false,
+//     });
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleSubmit(newMovie) {
+//     movieAPI.createMovie(newMovie);
+//     this.setState({
+//       shouldRedirect: true,
+//     });
+//   }
+
+//   render() {
+//     const { shouldRedirect } = this.state;
+//     return (
+//       shouldRedirect ? <Redirect to="/" /> : (
+//         <div data-testid="new-movie">
+//           <MovieForm onSubmit={ this.handleSubmit } />
+//         </div>
+//       )
+//     );
+//   }
+// }
+// export default NewMovie;
 import React, { Component } from 'react';
 
 import MovieForm from '../components/MovieForm';
-import * as movieAPI from '../services/movieAPI';
+// import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
@@ -9,7 +43,7 @@ class NewMovie extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(newMovie) {
+  handleSubmit() {
   }
 
   render() {
